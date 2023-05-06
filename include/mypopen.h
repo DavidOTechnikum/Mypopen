5a98dd0d66c5f5f2e501cd54bea0e15705f165e5
DavidOTechnikum <128523550+DavidOTechnikum@users.noreply.github.com>
2023-05-06 11:27:19 +0200

    Add files via upload

---
+++ include/mypopen.h
@@ -0,0 +1,7 @@
+#ifndef B0436966_67FC_498E_A108_D6120BEAE275
+#define B0436966_67FC_498E_A108_D6120BEAE275
+
+FILE *mypopen(const char *command, const char *type);
+int mypclose(FILE *stream);
+
+#endif /* B0436966_67FC_498E_A108_D6120BEAE275 */
